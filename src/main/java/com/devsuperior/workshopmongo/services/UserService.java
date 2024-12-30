3f53c3ceac2a52ab4b43515e7a75a70afec3cd3e
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-12-30 16:51:34 -0300

    POST New User

--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -2,8 +2,10 @@ package com.devsuperior.workshopmongo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.workshopmongo.dto.UserDTO;
+import com.devsuperior.workshopmongo.entities.User;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
 import com.devsuperior.workshopmongo.services.exceptions.ResourceNotFoundException;
 
@@ -26,22 +28,14 @@ public class UserService {
 				.map(existingUser -> (new UserDTO(existingUser)))
 				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Recurso não encontrado!")));
 	}
-	/*
-	@Transactional(readOnly = true)
-	public List<PostDTO> findPosts(String id) {
-		User user = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		List<PostDTO> result = user.getPosts().stream().map(x -> new PostDTO(x)).toList();
-		return result;
-	}
-
-	@Transactional
-	public UserDTO insert(UserDTO dto) {
+	
+	public Mono<UserDTO> insert(UserDTO dto) {
 		User entity = new User();
 		copyDtoToEntity(dto, entity);
-		entity = repository.save(entity);
-		return new UserDTO(entity);
+		Mono<UserDTO> result = repository.save(entity).map(user -> (new UserDTO(user)));
+		return result;
 	}
-
+	/*
 	@Transactional
 	public UserDTO update(String id, UserDTO dto) {
 		User entity = repository.findById(id)
@@ -57,10 +51,10 @@ public class UserService {
 				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
 		repository.delete(entity);
 	}
-
+*/
 	private void copyDtoToEntity(UserDTO dto, User entity) {
 		entity.setName(dto.getName());
 		entity.setEmail(dto.getEmail());
 	}
-	*/
+
 }
